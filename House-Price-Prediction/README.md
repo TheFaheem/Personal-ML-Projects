1fe58baf96171e703a5fbedf73271ee28150dbd4
Mohammed Faheem <104909089+TheFaheem@users.noreply.github.com>
2022-12-10 12:15:50 +0530

    Organized Projects

--- House-Price-Prediction/README.md
+++
@@ -1,1 +0,0 @@
-This Directory Contains My House Price Prediction Model Which I Have Built From Scratch, Without Using Any Library Only Using Basic Libraries Like Numpy.
--- House-Price-Prediction/utils.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-def load_data( ):
-	data = np.loadtxt("house_price_data.txt", delimiter=',')
-	X = data[:,0]
-	y = data[:,1]
-	return X, y
-	
-
-x_train, y_train = load_data() 
